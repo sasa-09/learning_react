ebfebc5e5a45611d2fa50092c26dc0ee15ce0b5a
sasa-09 <akollerov11@mail.ru>
2024-04-14 12:18:48 +0700

    helloReact

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <>
+    <h1>Hello React!</h1>
+    <ul>
+      <li>Aa</li>
+      <li>Bb</li>
+      <li>Cc</li>
+      <li>Dd</li>
+    </ul>
+  </>
+  );
+}
+
+export default App;
