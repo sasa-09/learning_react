d2036e83599c13ee9c7741d2d258d08ca6033afe
sasa <akollerov11@mail.ru>
2024-04-21 05:15:11 +0700

    components and props: task-2

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 import './style.css'
+import Hero from './Hero'
 
-function Hero () {
-    return (
-        <>
-          <h1>Добро пожаловать в наш магазин</h1>
-          <p>Начать покупки!</p>
-        </>
-    )
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
     <>
         <Hero />
     </>
-);
+)
 
